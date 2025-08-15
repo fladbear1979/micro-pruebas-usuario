aac4e1d325dfd386f447db3d978c3642a67efd4c
fladbear1979 <sfy6p390s1eh@hotmail.com>
2025-08-15 05:42:37 -0500

    feat: initial commit for src/index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Bienvenido a Micro Pruebas de Usuario');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
